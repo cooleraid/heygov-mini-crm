b721bafbb6ecee96f3ead8b04e0624bf50cdda6b
Adeoluwa Akinsanya <akinsanyaadeoluwa21@gmail.com>
2024-03-28 22:42:15 +0100

    Remove netlify config

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 const App = require('./app');
 const { validateEnv } = require('./utils/index.util');
-const serverless = require("serverless-http");
 
 validateEnv();
 
 const app = new App();
 
-module.exports.handler = serverless(app);
+app.listen();
